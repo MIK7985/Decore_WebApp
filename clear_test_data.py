c8b8aa8ff1bab388a4a06ea8ab408f04d7d2076c
MUHAMMED IRFAN K <mikwebsites@gmail.com>
2026-05-08 03:07:44 +0530

    Add script to wipe test data

---
+++ clear_test_data.py
@@ -0,0 +1,56 @@
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'decore_developers.settings')
+django.setup()
+
+from django.contrib.auth import get_user_model
+from employees.models import Employee
+from sites_mgmt.models import WorkSite, WorkArea, WorkAreaImage, SitePayment, EmployeeAssignment
+from attendance.models import Attendance
+from salary.models import SalarySummary, SalaryDetail, AdvanceRequest
+from inventory.models import Material, MaterialStock, DeliveryLog, MaterialRequest
+
+User = get_user_model()
+
+def clear_test_data():
+    print("WARNING: This will delete ALL data except Superuser accounts.")
+    confirm = input("Type 'YES' to continue: ")
+    
+    if confirm != 'YES':
+        print("Operation cancelled.")
+        return
+
+    print("Deleting Salary Records...")
+    SalaryDetail.objects.all().delete()
+    SalarySummary.objects.all().delete()
+    AdvanceRequest.objects.all().delete()
+
+    print("Deleting Attendance...")
+    Attendance.objects.all().delete()
+
+    print("Deleting Site Data & Payments...")
+    WorkAreaImage.objects.all().delete()
+    WorkArea.objects.all().delete()
+    SitePayment.objects.all().delete()
+    EmployeeAssignment.objects.all().delete()
+    WorkSite.objects.all().delete()
+
+    print("Deleting Inventory Data...")
+    DeliveryLog.objects.all().delete()
+    MaterialRequest.objects.all().delete()
+    MaterialStock.objects.all().delete()
+    Material.objects.all().delete()
+
+    print("Deleting Employees...")
+    Employee.objects.all().delete()
+
+    print("Deleting Non-Admin Users (Clients, Staff, etc.)...")
+    # Keep superusers
+    users_deleted, _ = User.objects.filter(is_superuser=False).delete()
+    print(f"Deleted {users_deleted} user accounts.")
+
+    print("\n✅ All test data has been completely wiped. Your admin account is safe!")
+
+if __name__ == '__main__':
+    clear_test_data()
